04b1144dcbd12d4645910fc6d35e8858721eae7b
Maksim Zemlyanikin <zemlyanikin.maksim@gmail.com>
2022-04-06 23:17:17 +0400

    Initial Commit

---
+++ Shared/SwiftUiCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiCounterApp.swift
+//  Shared
+//
+//  Created by Maksim Zemlyanikin on 06.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
